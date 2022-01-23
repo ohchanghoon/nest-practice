50c1416b32b3c2ff6b5d64a34550ac85aa73f6a8
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-23 22:22:41 +0900

    Add(changhoon/master) : typeorm 설정파일 root module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { MemberModule } from './member/member.module';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 // import { typeORMConfig } from './member/configs/typeorm.config';
 import { BoardsModule } from './boards/boards.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './boards/configs/typeorm.config';
 // 데코레이터는 클래스, 메서드 또는 속성에 대해 정의
 // 데코레이터를 선언하면 해당 정의된 속성을 주입받아 사용 가능
 @Module({
   // 앱 모듈은 앱컨트롤러와 앱서비스만 가지고 있는게 좋기 때문에
   // 이 자리에 있던 유저 컨트롤러와 서비스는 유저 모듈로 이동시킨다.
   // nest g mo users 명령어를 실행시키면 imports에 연결됨
-  imports: [UsersModule, MemberModule, BoardsModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    UsersModule,
+    MemberModule,
+    BoardsModule,
+  ],
   controllers: [],
   providers: [],
 })
