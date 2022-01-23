c815105ae266e05f60ffde838943108b1fcdda23
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-23 22:58:25 +0900

    Refactor(changhoon/master) : 오타 수정

--- src/boards/configs/typeorm.config.ts
+++ src/boards/configs/typeorm.config.ts
@@ -9,7 +9,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   database: 'board-app',
   // 엔티티를 이용해서 데이터베이스 테이블을 생성 -> 엔티티파일이 어디에 있는지 설명
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  // true갑승ㄹ 주면 애플리케이션 다시 실행 시 엔티티안에서
+  // true값을 주면 애플리케이션 다시 실행 시 엔티티안에서
   // 수정된 컬럼의 길이 타입 변경값등을 해당 테이븡르 Drop한 후 다시생성
   synchronize: true,
 };
