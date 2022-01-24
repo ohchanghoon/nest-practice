ac90d0480349adbe2a9b53e408800929b2ad1020
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-24 13:37:13 +0900

    Add(changhoon/master) : 회원가입, 로그인 구현

---
+++ src/auth/dto/auth-credential.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  //   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  //   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // 영문과 숫자만 가능한 유효성 체크
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only english and number',
+  })
+  password: string;
+}
